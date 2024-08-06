d4ca7eacbbbc5687435cf01313a4cf2173fd7b43
HernandezEduin <86664284+HernandezEduin@users.noreply.github.com>
2024-08-06 21:44:46 +0800

    Neo4j Graph Builder and Node Query
    Added code for building a graph on Neo4j, updating the node information, adding relationships between nodes, querying for a node, neighborhood nodes, and paths.

---
+++ graph_builder_rdf.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 13:30:28 2024
+
+@author: Eduin Hernandez
+"""
+
+from typing import Dict, List
+import json
+import pandas as pd
+import csv
+import numpy as np
+
+from utils.fb_wiki_graph import FbWikiGraph
+from utils.basic import load_json
+
+def load_rdf_valid(file_path: str) -> List[str]:
+    return np.loadtxt(file_path, dtype=str).tolist()
+    
+# Function to load MDI to Title mapping from CSV
+def load_rdf_info_mapping(file_path: str) -> Dict:
+    mapping = {}
+    with open(file_path, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            mapping[row['RDF']] = row
+    return mapping
+    
+if __name__ == '__main__':
+    # Constants for file paths
+    RDF_NODES = './data/modified_fbwiki_nodes.txt'
+    RDF_DATA_MAPPING = './data/category_mapping.csv'
+    RELATION_MAPPING = './data/unique_properties_valid.json'
+    FILE = './data/modified_triplet.txt'
+
+    #--------------------------------------------------------------------------
+    'Load the Data'
+    rdf_data_map = load_rdf_info_mapping(RDF_DATA_MAPPING)
+    relation_map = load_json(RELATION_MAPPING)
+    nodes = load_rdf_valid(RDF_NODES)
+    
+    
+    g = FbWikiGraph('bolt://localhost:7687', 'neo4j', '11082000')
+    
+    #--------------------------------------------------------------------------
+    'Empties and Create Graph'
+    # g.create_graph(rdf_valid) # Avoid using if a graph already exists as this will erase it
+
+    g.create_new_nodes(nodes)
+    
+    #--------------------------------------------------------------------------
+    'Update Node Information'
+    g.update_nodes_base_information(rdf_data_map)
+    g.update_node_category(rdf_data_map)
+    
+    #--------------------------------------------------------------------------
+    'Connect Triplets'
+    g.create_link_between_nodes(relation_map, FILE)
+    
--- utils/fb_wiki_graph.py
+++ utils/fb_wiki_graph.py
@@ -25,10 +25,21 @@ class FbWikiGraph():
 
     def get_drive(self):
         return GraphDatabase.driver(self.uri, auth=(self.user, self.password))
+    
     # Function to create nodes for each RDF title mapping
-    def create_nodes(self, tx, rdf_valid):
+    def _create_nodes(self, tx, rdf_valid) -> None:
         for rdf in tqdm(rdf_valid, desc='Creating nodes'):
             tx.run("CREATE (:Node {RDF: $rdf})", rdf=rdf)
+        
+    def _create_new_nodes(self, tx, rdf_valid: List[str]) -> None:
+        for rdf in tqdm(rdf_valid, desc='Checking and creating nodes'):
+            query = (
+                """
+                MERGE (n:Node {RDF: $rdf})
+                ON CREATE SET n.RDF = $rdf
+                """
+            )
+            tx.run(query, rdf=rdf)
     
     #--------------------------------------------------------------------------
     'Functions to Initialize and Modify Nodes + Relationships'
@@ -40,7 +51,15 @@ class FbWikiGraph():
             # Clear the graph before adding new data
             session.execute_write(self.clear_graph)
             # Create nodes for each RDF title mapping
-            session.execute_write(self.create_nodes, rdf_valid)
+            session.execute_write(self._create_nodes, rdf_valid)
+        driver.close()
+        
+    def create_new_nodes(self, rdf_valid: List[str]) -> None:
+        driver = self.get_drive()
+        
+        with driver.session() as session:
+            # Create nodes for each RDF title mapping
+            session.execute_write(self._create_new_nodes, rdf_valid)
         driver.close()
     
     def create_link_between_nodes(self, relation_map: Dict, file_name: str) -> None:
@@ -60,27 +79,40 @@ class FbWikiGraph():
                     
         driver.close()
     
-    def update_nodes_information(self, rdf_info_map: Dict) -> None:
+    def update_nodes_base_information(self, rdf_info_map: Dict) -> None:
         driver = self.get_drive()
         with driver.session() as session:
             for rdf, info in tqdm(rdf_info_map.items(), desc='Updating nodes'):
                 query = (
-                    "MATCH (n:Node {RDF: $rdf}) "
-                    "SET n += {"
-                    "Title: $title, "
-                    "Description: $description, "
-                    "MDI: $mdi, "
-                    "URL: $url, "
-                    "Alias: $alias"
-                    "}"
+                    """
+                    MATCH (n:Node {RDF: $rdf})
+                    SET n.Title = $info.Title,
+                        n.Description = $info.Description, 
+                        n.MDI = $info.MDI, 
+                        n.URL = $info.URL, 
+                        n.Alias = $info.Alias
+                    """
                 )
-                session.run(query, rdf=rdf, 
-                            title=info['Title'],
-                            description=info['Description'],
-                            mdi=info['MDI'],
-                            url=info['URL'],
-                            alias=info['Alias'])
+                session.run(query, rdf=rdf, info=info)
         driver.close()
+    
+    def update_node_category(self, rdf_info_map: Dict) -> None:
+        driver = self.get_drive()
+        with driver.session() as session:
+            for rdf, info in tqdm(rdf_info_map.items(), desc='Updating nodes'):
+                query = (
+                    """
+                    MATCH (n:Node {RDF: $rdf})
+                    UNWIND $categories_values AS cv
+                    SET n.Category = cv.Category,
+                        n.has_category = cv.has_category
+                    """
+                )
+                session.run(query, rdf=rdf,
+                            categories_values=info
+                            )
+        driver.close()
+        
     #--------------------------------------------------------------------------
     'Functions to Extract Nodes'
     def match_node(self, rdf: str) -> Dict:
@@ -125,3 +157,94 @@ class FbWikiGraph():
             
         driver.close()
         return nodes, rels
+    
+    def match_inwards(self, rdf: str) -> Tuple[List[Dict], List[Dict]]:
+        """ Returns the list of nodes and relations that are going into the input node.
+        MUST BE IN RDF FORMAT: i.e.: Q76 is the RDF code for Barack Obama"""
+        assert 'Q' in rdf
+        
+        driver = self.get_drive()
+        
+        nodes, rels = [], []
+        with driver.session() as session:
+            query = ("MATCH (n:Node {RDF: $rdf})<-[r]-(connected)"
+                     "RETURN r, connected")
+            nod_rel  = session.run(query, rdf=rdf)
+            
+            if nod_rel.peek():
+                for record in nod_rel:
+                    # Extract relationship properties
+                    rel_properties = dict(record['r'])
+                    rels.append(rel_properties)
+                    
+                    # Extract node properties
+                    node_properties = dict(record['connected'])
+                    nodes.append(node_properties)
+            
+        driver.close()
+        return nodes, rels
+    
+    def match_outwards(self, rdf: str) -> Tuple[List[Dict], List[Dict]]:
+        """ Returns the list of nodes and relations that are going out of the input node.
+        MUST BE IN RDF FORMAT: i.e.: Q76 is the RDF code for Barack Obama"""
+        assert 'Q' in rdf
+        
+        driver = self.get_drive()
+        
+        nodes, rels = [], []
+        with driver.session() as session:
+            query = ("MATCH (n:Node {RDF: $rdf})-[r]->(connected)"
+                     "RETURN r, connected")
+            nod_rel  = session.run(query, rdf=rdf)
+            
+            if nod_rel.peek():
+                for record in nod_rel:
+                    # Extract relationship properties
+                    rel_properties = dict(record['r'])
+                    rels.append(rel_properties)
+                    
+                    # Extract node properties
+                    node_properties = dict(record['connected'])
+                    nodes.append(node_properties)
+            
+        driver.close()
+        return nodes, rels
+    
+    def find_path(self, rdf_start: str, rdf_end: str, min_hops: int = 2, max_hops: int = 3, limit: int = 1) -> List[Tuple[List[Dict], List[Dict]]]:
+        """
+        Finds multiple paths between two nodes identified by RDF, within a specified hop range.
+        
+        Args:
+            rdf_start (str): The RDF identifier for the start node.
+            rdf_end (str): The RDF identifier for the end node.
+            min_hops (int): Minimum number of hops in the path (default is 2).
+            max_hops (int): Maximum number of hops in the path (default is 3).
+            limit (int): Max number of paths to generate (default is 1).
+        
+        Returns:
+            Tuple[List[Dict], List[Dict]]: A tuple containing a list of nodes and relationships in the path.
+        """
+        assert 'Q' in rdf_start
+        assert 'Q' in rdf_end
+    
+        driver = self.get_drive()
+        
+        paths = []
+        with driver.session() as session:
+            query = (
+                f"""
+                MATCH path = (n {{RDF: $rdf_start}})-[*{min_hops}..{max_hops}]->(m {{RDF: $rdf_end}})
+                RETURN nodes(path) AS nodes, relationships(path) AS relationships
+                LIMIT $limit
+                """
+            )
+            result = session.run(query, rdf_start=rdf_start, rdf_end=rdf_end, limit=limit)
+            
+            if result.peek():
+                for record in result:
+                    nodes = [dict(node) for node in record['nodes']]
+                    relationships = [dict(rel) for rel in record['relationships']]
+                    paths.append((nodes, relationships))
+        
+        driver.close()
+        return paths
