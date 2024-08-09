a638c848b3ca23641ce78fe838f45647dbd497ac
HernandezEduin <86664284+HernandezEduin@users.noreply.github.com>
2024-08-09 20:13:56 +0800

    Testing python-neo4j path extraction

--- triplet_creations/graph_inspect_rdf.py
+++ triplet_creations/graph_inspect_rdf.py
@@ -3,7 +3,7 @@
 Created on Mon Jul 15 22:39:04 2024
 
 @author: Eduin Hernandez
-Sample code to extract matching nodes and neighborhood
+Sample code to extract matching nodes, neighborhood, and paths
 """
 from utils.fb_wiki_graph import FbWikiGraph
 from utils.configs import global_configs
@@ -17,6 +17,15 @@ def print_paths(paths) -> None:
             path_vis += ' --' + rels[i0-1]['Title'] + '--> ' + nodes[i0]['Title']
         print(path_vis)
 
+def print_paths_rdf(paths) -> None:
+    for path in paths:
+        nodes = path[0]
+        rels = path[1]
+        path_vis = 'Hop size of ' + str(len(nodes) - 1) + ': ' + nodes[0]
+        for i0 in range(1, len(nodes)):
+            path_vis += ' --' + rels[i0-1] + '--> ' + nodes[i0]
+        print(path_vis)
+    
 if __name__ == '__main__':
     
     configs = global_configs('./configs/configs.ini')
@@ -24,28 +33,31 @@ if __name__ == '__main__':
     
     g = FbWikiGraph(neo4j_parameters['uri'], neo4j_parameters['user'], neo4j_parameters['password'])
     
-    # #--------------------------------------------------------------------------
-    # 'Matching Nodes and Neighborhoods'
+    #--------------------------------------------------------------------------
+    'Matching Nodes and Neighborhoods'
     
-    # node = 'Q5'                             # human
+    node = 'Q76'                             # human
     
-    # nodeA = g.match_node(node)              # extracting node info
+    nodeA = g.match_node(node)              # extracting node info
     
-    # nodesB, relsB = g.match_connected(node) # extracting the neighborhood of parent node
+    nodesB, relsB = g.match_related_nodes(node, direction='any') # extracting the neighborhood of parent node
     
-    # nodesC, relsC = g.match_outwards(node)  # extracting nodes to which this one points to
+    nodesC, relsC = g.match_related_nodes(node, direction='->')  # extracting nodes to which this one points to
     
-    # nodesD, relsD = g.match_inwards(node)   # extracting nodes to which point to this one
+    nodesD, relsD = g.match_related_nodes(node, direction='<-', rdf_only=True)   # extracting nodes to which point to this one
     
-    # #_-------------------------------------------------------------------------
-    # 'Paths'
+    #--------------------------------------------------------------------------
+    'Paths'
     
-    # start_node = 'Q17'                      # Japan
-    # end_node   = 'Q183'                     # Germany
-    # min_hops = 2
-    # max_hops = 3
-    # limit = 100
+    start_node = 'Q17'                      # Japan
+    end_node   = 'Q183'                     # Germany
+    min_hops = 2
+    max_hops = 3
+    limit = 100
     
-    # paths = g.find_path(start_node, end_node, min_hops=min_hops, max_hops=max_hops, limit=limit)
-    
-    # print_paths(paths)
+    paths = g.find_path(start_node, end_node, min_hops=min_hops, max_hops=max_hops, limit=limit)
+    print_paths(paths)
+
+    rels = ['participant_in', 'member_of', 'shares_borders_with']
+    paths = g.find_path(start_node, end_node, min_hops=min_hops, max_hops=max_hops, limit=limit, relationship_types=rels, rdf_only=True)
+    print_paths_rdf(paths)
