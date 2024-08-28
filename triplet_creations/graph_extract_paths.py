cddd80fcefd0443ca6f6bbc2955befa6c71f6c59
HernandezEduin <86664284+HernandezEduin@users.noreply.github.com>
2024-08-28 14:21:06 +0800

    Relationship Pruning from Paths
    Added option to exclude non-informative relationship from the path extraction.

--- triplet_creations/graph_extract_paths.py
+++ triplet_creations/graph_extract_paths.py
@@ -50,7 +50,8 @@ def parse_args():
     parser.add_argument('--max-hops', type=int, default=3, help='Maximum number of hops to consider in the path.')
     parser.add_argument('--max-attempts', type=int, default=1E9, help='Max number of attempts for extracting paths.')
     parser.add_argument('--num-workers', type=int, default=10, help='Number of workers to use for path extractions.')
-    parser.add_argument('--use-filter', type=str2bool, default='True', help='Whether to filter out the path by using the relationships.')
+    parser.add_argument('--use-filter', type=str2bool, default='False', help='Whether to filter out the path by using the relationships.')
+    parser.add_argument('--use-pruning', type=str2bool, default='True', help='Whether to remove non-informative relationships.')
     parser.add_argument('--use-rand-path', type=str2bool, default='False', help='Whether to search and return paths randomly between two pairs. Warning! This could slow down path finding algorithms.')
     parser.add_argument('--path-per-pair', type=int, default=1, help='Maximum number of paths to generate per head and tail pairing.')
     parser.add_argument('--total-paths', type=int, default=2E4, help='Total of number of paths to create.')
@@ -69,12 +70,15 @@ def process_pair(g, x, y, args, nrfilter):
     Function to process a pair (x, y) and return the paths found.
     """
     rels = None
-    if args.use_filter: rels = nrfilter.nodes_rel_filters(x, y)
+    non_inform = []
+    if args.use_filter: rels = nrfilter.nodes_rel_filters(x, y, remove_noninformative = args.use_pruning)
+    if args.use_pruning and not(args.use_filter): non_inform = nrfilter.get_noninform_rels(neo4j = True)
     
     paths = g.find_path(x, y, 
                         min_hops=args.min_hops,
                         max_hops=args.max_hops, 
-                        relationship_types=rels, 
+                        relationship_types=rels,
+                        noninformative_types=non_inform,
                         limit=args.path_per_pair,
                         rdf_only=True,
                         rand = args.use_rand_path
@@ -122,96 +126,107 @@ if __name__ == '__main__':
     
     g = FbWikiGraph(neo4j_parameters['uri'], neo4j_parameters['user'], neo4j_parameters['password'])
     
-    # #--------------------------------------------------------------------------
-    # 'Example of how to extract paths'
-    # start_node = 'Q76' # Barack Obama
-    # end_node = 'Q9682' # Elizabeth II
-    # rels = nrfilter.nodes_rel_filters(start_node, end_node)
-    # paths = g.find_path(start_node, end_node, min_hops=args.min_hops, max_hops=args.max_hops,
-    #                     limit=args.path_per_pair, relationship_types=rels, rdf_only=True)
-
     #--------------------------------------------------------------------------
-    'Extracting Multihop Paths'    
+    'Example of how to extract paths'
+    start_node = 'Q76' # Barack Obama
+    end_node = 'Q9682' # Elizabeth II
+    rels = None
+    non_inform = []
+    if args.use_filter: rels = nrfilter.nodes_rel_filters(start_node, end_node, remove_noninformative = args.use_pruning)
+    if args.use_pruning and not(args.use_filter): non_inform = nrfilter.get_noninform_rels(neo4j = True)
     
-    write_path = args.dataset_folder + args.dataset_prefix + '_hop'
-    write_path += f"_{args.dataset_suffix}.csv" if args.dataset_suffix else '.csv'
-    success_path = fail_path = args.dataset_folder + args.dataset_prefix 
-    success_path += f"_{args.dataset_suffix}_success.txt" if args.dataset_suffix else '_success.txt'
-    fail_path += f"_{args.dataset_suffix}_fails.txt" if args.dataset_suffix else '_fails.txt'
+    paths = g.find_path(start_node, end_node, 
+                        min_hops=args.min_hops,
+                        max_hops=args.max_hops, 
+                        relationship_types=rels,
+                        noninformative_types=non_inform,
+                        limit=args.path_per_pair,
+                        rdf_only=True,
+                        rand = args.use_rand_path
+                        )
 
-    pair_set = set()
-    path_count = 0
-    attempts = 0
+    # #--------------------------------------------------------------------------
+    # 'Extracting Multihop Paths'    
     
-    if args.reload_set: # Reuse previous information of pairs to continue
-        assert os.path.isfile(write_path), 'Error! Dataset file has not been created yet!'
-        assert os.path.isfile(success_path), 'Error! Success file has not been created yet!'
-        assert os.path.isfile(fail_path), 'Error! Fail file has not been created yet!'
+    # write_path = args.dataset_folder + args.dataset_prefix + '_hop'
+    # write_path += f"_{args.dataset_suffix}.csv" if args.dataset_suffix else '.csv'
+    # success_path = fail_path = args.dataset_folder + args.dataset_prefix 
+    # success_path += f"_{args.dataset_suffix}_success.txt" if args.dataset_suffix else '_success.txt'
+    # fail_path += f"_{args.dataset_suffix}_fails.txt" if args.dataset_suffix else '_fails.txt'
 
-        f = open(success_path, "r")
-        for i0 in f:
-            pair_set.add(i0)
-            attempts += 1
-            path_count +=1
+    # pair_set = set()
+    # path_count = 0
+    # attempts = 0
+    
+    # if args.reload_set: # Reuse previous information of pairs to continue
+    #     assert os.path.isfile(write_path), 'Error! Dataset file has not been created yet!'
+    #     assert os.path.isfile(success_path), 'Error! Success file has not been created yet!'
+    #     assert os.path.isfile(fail_path), 'Error! Fail file has not been created yet!'
+
+    #     f = open(success_path, "r")
+    #     for i0 in f:
+    #         pair_set.add(i0)
+    #         attempts += 1
+    #         path_count +=1
             
-        f = open(fail_path, "r")
-        for i0 in f:
-            pair_set.add(i0)
-            attempts += 1
-    else: # Start fresh
-        with open(write_path, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(column_names)  # Write the column names as the header row
+    #     f = open(fail_path, "r")
+    #     for i0 in f:
+    #         pair_set.add(i0)
+    #         attempts += 1
+    # else: # Start fresh
+    #     with open(write_path, mode='w', newline='') as file:
+    #         writer = csv.writer(file)
+    #         writer.writerow(column_names)  # Write the column names as the header row
         
-        with open(fail_path, 'w') as f:
-            pass
+    #     with open(fail_path, 'w') as f:
+    #         pass
         
-        with open(success_path, 'w') as f:
-            pass
+    #     with open(success_path, 'w') as f:
+    #         pass
     
-    # Pre-open the CSV file for writing
-    with open(write_path, mode='a', newline='') as f:
-        writer = csv.writer(f)
+    # # Pre-open the CSV file for writing
+    # with open(write_path, mode='a', newline='') as f:
+    #     writer = csv.writer(f)
         
-        # Initialize tqdm progress bar for the main loop
-        with tqdm(total=int(args.total_paths) - path_count, desc="Generating paths", unit="path") as pbar:
-            with ThreadPoolExecutor(max_workers=args.num_workers) as executor:  # Adjust max_workers based on your system
-                while path_count < args.total_paths and attempts < args.max_attempts:
-                    # Generate and submit tasks in a batch
-                    future_to_pair = {}
+    #     # Initialize tqdm progress bar for the main loop
+    #     with tqdm(total=int(args.total_paths) - path_count, desc="Generating paths", unit="path") as pbar:
+    #         with ThreadPoolExecutor(max_workers=args.num_workers) as executor:  # Adjust max_workers based on your system
+    #             while path_count < args.total_paths and attempts < args.max_attempts:
+    #                 # Generate and submit tasks in a batch
+    #                 future_to_pair = {}
 
-                    while len(future_to_pair) < args.num_workers and path_count < args.total_paths:
-                        attempts += 1
+    #                 while len(future_to_pair) < args.num_workers and path_count < args.total_paths:
+    #                     attempts += 1
                     
-                        x, y = random.sample(nodes_list, 2)  # prevents x == y
-                        p = x + '_' + y
-                        if p in pair_set: continue
-                        pair_set.add(p)
+    #                     x, y = random.sample(nodes_list, 2)  # prevents x == y
+    #                     p = x + '_' + y
+    #                     if p in pair_set: continue
+    #                     pair_set.add(p)
     
-                        # Submit the process_pair task to the thread pool
-                        future = executor.submit(process_pair, g, x, y, args, nrfilter)
-                        future_to_pair[future] = (x, y)
+    #                     # Submit the process_pair task to the thread pool
+    #                     future = executor.submit(process_pair, g, x, y, args, nrfilter)
+    #                     future_to_pair[future] = (x, y)
     
-                    # Process the completed futures as they finish
-                    for future in as_completed(future_to_pair):
-                        paths, x, y = future.result()
-                        p = x + '_' + y + '\n'
-                        for path in paths:
-                            tmp_path = [
-                                item for pair in zip_longest(path[0], path[1])
-                                for item in pair if item is not None
-                            ]
-                            writer.writerow(tmp_path)
-                            # f.flush()
+    #                 # Process the completed futures as they finish
+    #                 for future in as_completed(future_to_pair):
+    #                     paths, x, y = future.result()
+    #                     p = x + '_' + y + '\n'
+    #                     for path in paths:
+    #                         tmp_path = [
+    #                             item for pair in zip_longest(path[0], path[1])
+    #                             for item in pair if item is not None
+    #                         ]
+    #                         writer.writerow(tmp_path)
+    #                         # f.flush()
                             
-                            path_count += 1
-                            pbar.update(1)
+    #                         path_count += 1
+    #                         pbar.update(1)
                         
-                        if paths:
-                            with open(success_path, 'a') as f:
-                                f.write(p + '')
-                        else:
-                            with open(fail_path, 'a') as f:
-                                f.write(p)
-                        # Break the loop if we have reached the required path count
-                        if path_count >= args.total_paths: break
+    #                     if paths:
+    #                         with open(success_path, 'a') as f:
+    #                             f.write(p + '')
+    #                     else:
+    #                         with open(fail_path, 'a') as f:
+    #                             f.write(p)
+    #                     # Break the loop if we have reached the required path count
+    #                     if path_count >= args.total_paths: break
