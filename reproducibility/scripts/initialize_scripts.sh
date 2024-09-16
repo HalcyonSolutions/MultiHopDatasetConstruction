70066c4446fa185dee4bd66f49d31dce90b75ea2
Luis Garcia <luisgarcia26284@gmail.com>
2024-09-16 16:47:57 +0800

    Improved: Service Account Initialization

--- reproducibility/scripts/initialize_scripts.sh
+++ reproducibility/scripts/initialize_scripts.sh
@@ -15,16 +15,16 @@ for file in $LIST_OF_GPG_FILES; do
         exit 1
       fi
       echo "$RESULT" > $name_without_gpg
+      # If We have service_account.json then we use it to auth 
+      base_name=$(basename $name_without_gpg)
+      if [ "$base_name" == "service_account.json" ]; then
+        echo -e "\033[0;33m::Found service account ${name_without_gpg}. Activating service account\033[0m"
+        gcloud auth activate-service-account --key-file="$name_without_gpg" &> /dev/null
+        ACCOUNT_NAME=$(cat "$name_without_gpg" | jq -r '.client_email')
+        echo -e "\033[0;32m::Activated service account $ACCOUNT_NAME\033[0m"
+        gcloud config set account $ACCOUNT_NAME
+        export ACCOUNT_NAME=$(gcloud config get-value account)
+      fi
     fi
 done
-
-if [ ! -f "service_account.json" ]; then
-  echo -e "\033[0;31m::No service account found. Please create one and try again\033[0m"
-  exit 1
-fi
-gcloud auth activate-service-account --key-file=service_account.json &> /dev/null
-ACCOUNT_NAME=$(cat service_account.json | jq -r '.client_email')
-echo -e "\033[0;32m::Activated service account $ACCOUNT_NAME\033[0m"
-gcloud config set account $ACCOUNT_NAME
-export ACCOUNT_NAME=$(gcloud config get-value account)
 
