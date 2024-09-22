fbeeef8f2f075ef56b57465a194b4798f286752c
Nurassyl-lab <nurassyl.askar@gmail.com>
2024-09-22 19:30:07 +0800

    Update: Unfinished DataLoader needs tokenizer

--- TriviaQA_DataLoaders/FbWiki_TriviaQA_DataLoader.py
+++ TriviaQA_DataLoaders/FbWiki_TriviaQA_DataLoader.py
@@ -5,7 +5,7 @@ from torchvision import datasets, transforms
 import pandas as pd
 
 # Define a custom dataset class (if needed)
-class CustomDataset(Dataset):
+class FbWikiDataset(Dataset):
     def __init__(self, dataset, n_hops=2, transform=None):
         self.data = pd.read_csv(dataset)[:100]
         self.info = {'dataset':dataset, 'n_hops':n_hops}
@@ -18,12 +18,18 @@ class CustomDataset(Dataset):
         path = row[:(2*self.info['n_hops']+1)].values
         question = row['question']
         answer = row['answer']
-        return path, question, answer
+
+        # ! TODO: Convert info into tensors
+        question = tokenize(...)
+        answer = tokenize(...)       
+        path = [tokenize(...) for p in path]
+
+        return data 
 
 # Function to create a DataLoader
 def create_dataloader(dataset, n_hops, batch_size=32, shuffle=True, num_workers=0):
     # Create the dataset
-    dataset = CustomDataset(dataset=dataset, n_hops=n_hops)
+    dataset = FbWikiDataset(dataset=dataset, n_hops=n_hops)
 
     # Create the DataLoader
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
@@ -35,6 +41,6 @@ if __name__ == "__main__":
     dataset = 'datasets/FbWiki_TriviaQA.csv'
     n_hops = 2
 
-    dataloader = create_dataloader(dataset, n_hops)
-    for path, question, answer in dataloader:
-        print(path, question, answer)
+    dataloader = create_dataloader(dataset, n_hops, batch_size=2, shuffle=False, num_workers=1)
+    for data in dataloader:
+        print(data)
