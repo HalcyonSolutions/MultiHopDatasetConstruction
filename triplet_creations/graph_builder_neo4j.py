dd40c2fa5945316339b941ff87bc410a876f4736
HernandezEduin <86664284+HernandezEduin@users.noreply.github.com>
2025-02-28 21:30:27 +0800

    Fb15k to Neo4j
    Added FB15k compatible graph building

--- triplet_creations/graph_builder_neo4j.py
+++ triplet_creations/graph_builder_neo4j.py
@@ -10,23 +10,37 @@ Summary: Makes a connection to Neo4j to upload triplet, entity, and relationship
 import argparse
 
 from utils.fb_wiki_graph import FbWikiGraph
-from utils.basic import load_pandas, load_to_set, str2bool
+from utils.fb_graph import FbGraph
+from utils.basic import load_pandas, load_to_set, load_triplets, str2bool
 from utils.configs import global_configs
 
 def parse_args():
     # Set up argument parsing
     parser = argparse.ArgumentParser(description="Process entities data for Neo4j graph creation.")
     
-    # Input arguments
-    parser.add_argument('--entity-list-path', type=str, default='./data/nodes_fb_wiki.txt',
+    # Sample for FbWiki Graph
+    # parser.add_argument('--entity-list-path', type=str, default='./data/nodes_fb_wiki.txt',
+    #                     help='Path to the list of entities.')
+    # parser.add_argument('--entity-data-path', type=str, default='./data/node_data_fb_wiki.csv',
+    #                     help='Path to the data of the entities.')
+    # parser.add_argument('--relationship-data-path', type=str, default='./data/relation_data_wiki.csv',
+    #                     help='Path to the data of the relationship.')
+    # parser.add_argument('--triplets-data-path', type=str, default='./data/triplets_fb_wiki.txt',
+    #                     help='Path to the relationship between entities.')
+
+    # Sample for Fb15k Graph
+    parser.add_argument('--entity-list-path', type=str, default='',
                         help='Path to the list of entities.')
-    parser.add_argument('--entity-data-path', type=str, default='./data/node_data_fb_wiki.csv',
+    parser.add_argument('--entity-data-path', type=str, default='./data/node_data_fb15k.csv',
                         help='Path to the data of the entities.')
-    parser.add_argument('--relationship-data-path', type=str, default='./data/relation_data_wiki.csv',
+    parser.add_argument('--relationship-data-path', type=str, default='./data/relation_data_fb15k.csv',
                         help='Path to the data of the relationship.')
-    parser.add_argument('--triplets-data-path', type=str, default='./data/triplets_fb_wiki.txt',
+    parser.add_argument('--triplets-data-path', type=str, default='./data/triplets_fb15k.txt',
                         help='Path to the relationship between entities.')
     
+    parser.add_argument('--freebase-compatible', type=str2bool, default='True',
+                        help='Whether the data is in Freebase format or Wikidata format.')
+
     parser.add_argument('--max-workers', type=int, default=10, 
                         help='Number of workers to connect to Neo4j')
     parser.add_argument('--batch-size', type=int, default=100, 
@@ -35,15 +49,14 @@ def parse_args():
     # Neo4j
     parser.add_argument('--config-path', type=str, default='./configs/configs.ini',
                         help='Path to the configuration file for Neo4j connection.')
-    parser.add_argument('--database', type=str, default='fbwiki',
+    parser.add_argument('--database', type=str, default='fb15k',
                         help='Name of the Neo4j database to use.')
     
-    parser.add_argument('--create-new-graph', type=str2bool, default='True',
+    parser.add_argument('--create-new-graph', type=str2bool, default='False',
                         help='Whether to clear the graph and add the nodes from scratch')
-    
-    parser.add_argument('--add-new-nodes', type=str2bool, default='True',
+    parser.add_argument('--add-new-nodes', type=str2bool, default='False',
                         help='Whether to add only the nodes')
-    parser.add_argument('--update-nodes-info', type=str2bool, default='True',
+    parser.add_argument('--update-nodes-info', type=str2bool, default='False',
                         help='Whether to update the details of the nodes')
     parser.add_argument('--upload-triplets', type=str2bool, default='True',
                         help='Whether to upload the links between the nodes')
@@ -56,10 +69,18 @@ if __name__ == '__main__':
 
     #--------------------------------------------------------------------------
     'Load the Data'
-    rdf_data_map = load_pandas(args.entity_data_path)
+    node_data_map = load_pandas(args.entity_data_path)
     relation_map = load_pandas(args.relationship_data_path)
-    nodes = list(load_to_set(args.entity_list_path))
-    
+    if 'Property' not in relation_map.columns:
+        relation_map['Property'] = ['r' + str(i + 1) for i in range(len(relation_map))] # Enumarate the relations for Neo4j compatibility
+        relation_map.to_csv(args.relationship_data_path.replace('.csv','') + '_processed.csv', index=False)
+
+    if args.entity_list_path:
+        nodes = list(load_to_set(args.entity_list_path))
+    else:
+        triplet_df = load_triplets(args.triplets_data_path)
+        nodes = list(set(triplet_df['head']) | set(triplet_df['tail']))
+
     configs = global_configs('./configs/configs.ini')
     neo4j_parameters = configs['Neo4j']
     
@@ -67,12 +88,18 @@ if __name__ == '__main__':
     'Process the Data'
     
     relation_map = relation_map.fillna('')
-    rdf_data_map = rdf_data_map.fillna('')
+    node_data_map = node_data_map.fillna('')
     
+    if args.freebase_compatible:
+        graph_builder = FbGraph
+    else:
+        graph_builder = FbWikiGraph
+
+
     #--------------------------------------------------------------------------
     'Create Connection with Neo4j'
-    g = FbWikiGraph(neo4j_parameters['uri'], neo4j_parameters['user'],
-                    neo4j_parameters['password'], database = args.database)
+    g = graph_builder(neo4j_parameters['uri'], neo4j_parameters['user'],
+                      neo4j_parameters['password'], database = args.database)
     
 
     #--------------------------------------------------------------------------
@@ -86,7 +113,7 @@ if __name__ == '__main__':
     'Update Node Information'
     if args.update_nodes_info:
         g.update_nodes_base_information(
-            rdf_data_map,
+            node_data_map,
             max_workers=args.max_workers, 
             batch_size=args.batch_size)
         
