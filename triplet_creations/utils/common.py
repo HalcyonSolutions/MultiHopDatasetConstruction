698d5f48789e286d412d8226be4d2bb06b831acf
ottersome <luisgarcia26284@gmail.com>
2025-06-30 22:59:44 -0600

    Adding: supporting datastructures

---
+++ triplet_creations/utils/common.py
@@ -0,0 +1,8 @@
+from typing import List, Tuple
+
+# StrTriplet = NewType("StrTriplet", tuple[str,str,str])
+StrTriplet = tuple[str,str,str]
+StrTripletWQualifier = tuple[str,str,str, ]
+
+Path = Tuple[List[any], List[any]]
+
